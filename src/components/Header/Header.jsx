eb3c1a4607ca0409a2fcb4a32a46c4f777b4edf3
chunmingyu <gyu4016@gmail.com>
2023-11-10 17:47:31 +0900

    base files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MemberList from "./MemberList";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <>
+      <h1>악동뮤지션 팬레터 콜렉션</h1>
+      <MemberList />
+    </>
+  );
+}
+
+export default Header;
